7b6447125eecabcb2555776e5f6a2ebb57e260cd
James Niyongira <jamesme85.me@gmail.com>
2023-01-11 17:38:09 -0500

    scafolding the register api

--- server/index.js
+++ server/index.js
@@ -8,6 +8,7 @@ import morgan from "morgan";
 import multer from "multer";
 import path from "path";
 import { fileURLToPath } from "url";
+import authRoutes from "./routes/auth.js";
 import { register } from "./controllers/auth.js";
 
 // CONFIGURATIONS OF MIDDLEWARES AND PACKAGES
@@ -37,6 +38,9 @@ const upload = multer({ storage });
 
 // ROUTES WITH FILES
 app.post("/auth/register", upload.single("picture"), register);
+
+// ROUTES WITHOUT FILES
+app.use("/auth", authRoutes);
 
 // MONGOOSE SETUP
 
